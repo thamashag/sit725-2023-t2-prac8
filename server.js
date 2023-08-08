12552a4506b2c2ab4667940fafd6ae3262a7849f
Thamasha Galahena <thamasha1996@gmail.com>
2023-08-08 21:03:47 +1000

    minimum requirements

--- server.js
+++ server.js
@@ -1,28 +1,68 @@
 let express = require('express');
 let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.jwmt60j.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res)=>{
-    res.render('index.html');
-})
-
-app.get('/addTwoNumbers', (req, res)=>{
-    //get values from url parameters
-    //addTwoNumbers?number1=1&number2=2
-    let num1 = req.query.number1;
-    let num2 = req.query.number2;
-
-    let sum = parseInt(num1)+parseInt(num2);
-
-    //send result
-    let obj = {statusCode:200, message:'success', data:sum}
-
-    res.json(obj);
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-app.listen(port, ()=>{
-    console.log('server started mate!');
 
-}); //logic that will initiate when servers starts
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch(ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', function (req,res) {
+    res.render('index.html');
+});
+
+app.get('/api/cats', (req,res) => {
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200, data:result, message:'get all cats successful'});
+        }
+    });
+});
+
+app.post('/api/cat', (req,res)=>{
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+});
+
+function postCat(cat,callback) {
+    collection.insertOne(cat,callback);
+}
+
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
+
+app.listen(port, ()=>{
+    console.log('express server started');
+    runDBConnection();
+});
+
+
+
+
+
